a65ca8514798dae3e1925d1cbbdf3bcb5cfaf8ca
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-09-12 19:32:31 +0600

    installed react with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1 className='text-red-600 text-5xl font-bold'>Knowledge cafe</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
